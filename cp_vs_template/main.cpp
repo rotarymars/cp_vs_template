587496a413de0ef1582124c2e8c7f7932e7f95c9
rotarymars <s.goto2050@gmail.com>
2023-11-02 01:02:03 +0900

    update cpp template

--- cp_vs_template/main.cpp
+++ cp_vs_template/main.cpp
@@ -2,8 +2,53 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
+#include <map>
+#include <set>
+#include <cmath>
 
 using namespace std;
+
+#define VALL(a) a.begin(), a.end()
+
+using ll = long long;
+using VI = std::vector<int>;
+using VVI = std::vector<VI>;
+using VC = std::vector<char>;
+using VVC = std::vector<VC>;
+
+int pt2n(const int x, const int y, const int w) { return (x * w) + y; }
+
+std::pair<int, int> num2pt(const int n, const int w) { return std::make_pair(n / w, n % w); }
+
+template <typename T>
+void PRINT1D(T a) {
+  cout << "******VECTOR 1D PRINT STARTED******\n";
+  cout << '{';
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    cout << a[i] << ", ";
+  }
+  cout << "}\n";
+  cout << "******VECTOR 1D PRINT ENDED******\n";
+}
+
+template <typename U>
+void PRINT2D(U a) {
+  cout << "******VECTOR 2D PRINT STARTED******\n";
+  cout << "{";
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    cout << "\n{";
+    for (size_t k = 0; k < a[i].size(); ++k)
+    {
+      cout << a[i][k] << ", ";
+    }
+    cout << "}, ";
+  }
+  cout << "\n}\n";
+  cout << "******VECTOR 2D PRINT ENDED******\n";
+}
 
 int main()
 {
